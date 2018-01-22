5a6e415e73ef5f751cb095794593b303e77015cd
Mike O'Donnell <mikeod38@gmail.com>
2018-01-22 17:36:03 -0500

    updated import function - auto read in

--- R/Import.WL.data.R
+++ R/Import.WL.data.R
@@ -3,6 +3,8 @@
 #' @param bin.length length of time bins in seconds. Used for state analysis
 #' @param frame.rate video frame rate
 #' @param num.tracks optional argument to limit input to certain number of worm tracks
+#' @importFrom magrittr %>%
+#' @importFrom magrittr %<>%
 #' @export
 #' @examples
 #' Import.WL.data(bin.length = 2, frame.rate = 6)
@@ -10,18 +12,26 @@
 Import.WL.data <- function(bin.length, frame.rate, num.tracks) {
 
   #### import data ### - use  readr to improve speed
-  message("Choose position data")
-  print(system.time(position <- read.csv(file.choose(), skip=4)))
-  message("Choose Direction data")
-  direction <- read.csv(file.choose(), skip=4)
-  message("Choose speed data")
-  speed <- read.csv(file.choose(), skip=4)
-  message("Choose length data")
-  length <- read.csv(file.choose(), skip=4)
-  message("Choose width data")
-  width <- read.csv(file.choose(), skip=4)
-  message("Choose Omega data")
-  state <- read.csv(file.choose(), skip=4)
+  message("Choose a position analysis file")
+  file <- file.choose()
+  file.pref <- basename(file) %>% strsplit(., c("Position","position")) %>% unlist()
+  folder <- dirname(file)
+  file_list  <- list.files(path = file.path(folder),pattern = file.pref[1])
+
+  position.path <- file.path(folder, file_list[grep("osition.csv",file_list)])
+  direction.path <- file.path(folder, file_list[grep("irection.csv",file_list)])
+  speed.path <- file.path(folder, file_list[grep("peed.csv",file_list)])
+  length.path <- file.path(folder, file_list[grep("ength.csv",file_list)])
+  width.path <- file.path(folder, file_list[grep("idth.csv",file_list)])
+  omega.path <- file.path(folder, file_list[grep("megaBend.csv",file_list)])
+
+  message("loading in data")
+  position <- read.csv(position.path, skip=4)
+  direction <- read.csv(direction.path, skip=4)
+  speed <- read.csv(speed.path, skip=4)
+  length <- read.csv(length.path, skip=4)
+  width <- read.csv(width.path, skip=4)
+  state <- read.csv(omega.path, skip=4)
 
 
   ####Setting up parameters####
@@ -40,38 +50,39 @@ Import.WL.data <- function(bin.length, frame.rate, num.tracks) {
 
   ######fxn to melt WL data to long format#########
   WL.pos.long <- function(data, num.tracks) {
-    subset.long <- data[,1:(num.tracks*2 + 2)] %>% melt(id.vars = c(1,2)) %>%
-      separate(variable, sep = "\\.", c("worm", "pos")) %>% dcast(Frame + Time + worm ~ pos)
+    subset.long <- data[,1:(num.tracks*2 + 2)] %>% reshape2::melt(id.vars = c(1,2)) %>%
+      tidyr::separate(variable, sep = "\\.", c("worm", "pos")) %>% reshape2::dcast(Frame + Time + worm ~ pos)
     return(subset.long)
   }
 
   ##### merge and get data ##########
-  print(system.time(WL.centroid <- WL.pos.long(position, num.tracks) %>% mutate(type = "centroid")))
+  message("merging datasets ...")
+  print(system.time(WL.centroid <- WL.pos.long(position, num.tracks) %>% dplyr::mutate(type = "centroid")))
 
   WL.speed <- speed[,1:(num.tracks + 2)] %>%
-    melt(id.vars = c(1,2)) %>%
-    separate(variable, sep = "\\.", c("worm", "stuffer")) %>%
-    rename(speed = value)
+    reshape2::melt(id.vars = c(1,2)) %>%
+    tidyr::separate(variable, sep = "\\.", c("worm", "stuffer")) %>%
+    dplyr::rename(speed = value)
 
   WL.head.dir <- direction[,1:(num.tracks + 2)] %>%
-    melt(id.vars = c(1,2)) %>%
-    separate(variable, sep = "\\.", c("worm", "stuffer")) %>%
-    rename(head.dir = value)
+    reshape2::melt(id.vars = c(1,2)) %>%
+    tidyr::separate(variable, sep = "\\.", c("worm", "stuffer")) %>%
+    dplyr::rename(head.dir = value)
 
   WL.length <- length[,1:(num.tracks + 2)] %>%
-    melt(id.vars = c(1,2)) %>%
-    separate(variable, sep = "\\.", c("worm", "stuffer")) %>%
-    rename(length = value)
+    reshape2::melt(id.vars = c(1,2)) %>%
+    tidyr::separate(variable, sep = "\\.", c("worm", "stuffer")) %>%
+    dplyr::rename(length = value)
 
   WL.width <- width[,1:(num.tracks + 2)] %>%
-    melt(id.vars = c(1,2)) %>%
-    separate(variable, sep = "\\.", c("worm", "stuffer")) %>%
-    rename(width = value)
+    reshape2::melt(id.vars = c(1,2)) %>%
+    tidyr::separate(variable, sep = "\\.", c("worm", "stuffer")) %>%
+    dplyr::rename(width = value)
 
   WL.state <- state[,1:(num.tracks + 2)] %>%
-    melt(id.vars = c(1,2)) %>%
-    separate(variable, sep = "\\.", c("worm", "stuffer")) %>%
-    rename(state = value)
+    reshape2::melt(id.vars = c(1,2)) %>%
+    tidyr::separate(variable, sep = "\\.", c("worm", "stuffer")) %>%
+    dplyr::rename(state = value)
 
   print(system.time(WL.alldata <- list(WL.centroid,
                                        WL.speed,
@@ -79,18 +90,73 @@ Import.WL.data <- function(bin.length, frame.rate, num.tracks) {
                                        WL.length,
                                        WL.width,
                                        WL.state)))
-  print(system.time(WL.alldata %<>% Reduce(function(...) dplyr::full_join(...), .)))
-  print(system.time(WL.alldata %<>% arrange(worm, Time)))
-  print(system.time(WL.alldata %<>% mutate(stuffer = NULL))) ### this takes longest
+  print(system.time(WL.alldata <- WL.alldata %>% Reduce(function(...) dplyr::full_join(...), .)))
+  print(system.time(WL.alldata <- WL.alldata %>% dplyr::arrange(worm, Time)))
+  print(system.time(WL.alldata <- WL.alldata %>% dplyr::mutate(stuffer = NULL))) ### this takes longest
 
-  print(system.time(WL.alldata <- WL.alldata %>% group_by(worm) %>%
-                      mutate(del.y2 =  y - lag(y), # change from previous point (t-1) to (t0)
-                             del.x2 = x - lag(x),
-                             del.x1 = lag(x) - lag(x, n=2), #vector from t(-2) to t(-1) for curve angle
-                             del.y1 = lag(y) - lag(y, n=2),
-                             time.bin = ntile(Time, n.bins),
-                             curve.ang = as.numeric(mapply(MF.matR::curve.angle, del.x1, del.y1, del.x2, del.y2))*180/pi) %>% group_by(worm, time.bin) %>%
-                      mutate(bin.speed = abs(mean(speed, na.rm=TRUE)), bin.ang.vel = mean(curve.ang, na.rm=TRUE)) %>% filter(!is.na(curve.ang))))
-  ###################################
+  message("calculating angular velocity (degrees/frame)")
+  print(system.time(
+    WL.alldata <- WL.alldata %>%
+      dplyr::group_by(worm) %>%
+      dplyr::mutate(del.y2 =  y - lag(y), # change from previous point (t-1) to (t0)
+             del.x2 = x - lag(x),
+             del.x1 = lag(x) - lag(x, n=2), #vector from t(-2) to t(-1) for curve angle
+             del.y1 = lag(y) - lag(y, n=2),
+             time.bin = dplyr::ntile(Time, n.bins),
+             curve.ang = as.numeric(mapply(MF.matR::curve.angle, del.x1, del.y1, del.x2, del.y2))*180/pi) %>%
+      dplyr::group_by(worm, time.bin) %>%
+      dplyr::mutate(bin.speed = abs(mean(speed, na.rm=TRUE)),
+             bin.ang.vel = mean(curve.ang, na.rm=TRUE)) %>%
+      dplyr::filter(!is.na(curve.ang))
+    ))
+
+  message("Correcting head direction")
+
+
+  #setting run direction threshold
+
+
+WL.alldata <- WL.alldata %>%
+                      dplyr::mutate(run.dir = dplyr::case_when(speed < -30 ~ -1,
+                                                               speed > 30 ~ 1,
+                                                               TRUE ~ 0))
+
+# using rle(), get length of runs in each direction, then get max in REVERSE direction
+backwards <- WL.alldata %>%
+  dplyr::group_by(worm) %>%
+  dplyr::do({tmp <- cbind(with(rle(.$run.dir==-1), lengths[values]))
+  data.frame(worm= .$worm, Max.back=if(length(tmp)==0) 0
+             else max(tmp)) }) %>% dplyr::slice(1L)
+
+# using rle(), get length of runs in each direction, then get max in FORWARD direction
+forward <- WL.alldata %>%
+  dplyr::group_by(worm) %>%
+  dplyr::do({tmp <- cbind(with(rle(.$run.dir==1), lengths[values]))
+  data.frame(worm= .$worm, Max.for=if(length(tmp)==0) 0
+             else max(tmp)) }) %>%
+  dplyr::slice(1L)
+
+max_runs <- dplyr::full_join(backwards,forward)
+
+# switch direction of worms that have longer runs in reverse than forward
+WL.alldata <- WL.alldata %>%
+  dplyr::mutate(speed = dplyr::case_when(
+    worm %in% max_runs[max_runs$Max.back > max_runs$Max.for,]$worm ~ -(speed),
+    TRUE ~ speed))
+#
+
+message("adding behavioral states")
+
+print(system.time(WL.alldata <- WL.alldata %>%
+  dplyr::mutate(state = dplyr::case_when(
+    state == "Omega Bend" ~ "omega",
+    abs(speed) < 30 ~ "pause",
+    curve.ang > 150 ~ "reversal",
+    speed < -30 ~ "backward",
+    TRUE ~ "forward"
+  ))))
+
+data.table::fwrite(WL.alldata, file.path(folder,paste0(file.pref[1],"all_track_data.csv")))
+
   return(WL.alldata)
 }
---
+++ R/plot_stateProbs.R
@@ -0,0 +1,40 @@
+#' Plot state probability from Wormlab dauer data by space and time.
+#' @param df data frame
+#' @param time_bin number of bins to split plot
+#' @param y_bin number of position bins
+#' @importFrom magrittr %>%
+#' @importFrom magrittr %<>%
+#' @export
+#' @examples
+#' plot_stateProbs <-
+#'
+
+plot_stateProbs <- function(df, time_bin, y_bin, nopause) {
+  if (missing(nopause)) {
+  df %>% dplyr::ungroup() %>%
+      dplyr::mutate(time.bin = dplyr::ntile(Time,time_bin),
+           y.bin = dplyr::ntile(y,y_bin),
+           state = factor(state)) %>%
+    dplyr::count(time.bin,y.bin,state) %>%
+    dplyr::group_by(time.bin,y.bin) %>%
+    dplyr::mutate(prop = prop.table(n)) %>%
+    ggplot(aes(x = y.bin, y = prop)) +
+    geom_point(aes(color = state)) + geom_smooth(aes(group = state, colour = state)) +
+      facet_wrap(~time.bin)
+  } else {
+    df %>% dplyr::ungroup() %>%
+      dplyr::mutate(time.bin = factor(dplyr::ntile(Time,time_bin)),
+                    y.bin = dplyr::ntile(y,y_bin),
+                    state = factor(state)) %>%
+      dplyr::count(time.bin,y.bin,state) %>%
+      dplyr::group_by(time.bin,y.bin) %>%
+      dplyr::mutate(prop = prop.table(n)) %>%
+      dplyr::filter(state != "pause") %>%
+      ggplot(aes(x = y.bin, y = prop)) +
+      geom_point(aes(color = state)) +
+      geom_smooth(aes(group = state, colour = state)) +
+      facet_wrap(~time.bin)
+  }
+
+}
+
